887e311e9a79d0ae31e39f61eec0c270ed53f95e
mstssk <mstssk+github@gmail.com>
2018-10-18 11:33:36 +0900

    リダイレクトを追従

--- setup-completion.sh
+++ setup-completion.sh
@@ -4,8 +4,8 @@ cd `dirname $0`
 
 # 参考: https://gist.github.com/juno/5546179
 cd ./.zsh/completion/
-curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
-curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh
+curl -L -O https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+curl -L -O https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh
 mv git-completion.zsh _git
 
 # https://docs.npmjs.com/cli/completion
