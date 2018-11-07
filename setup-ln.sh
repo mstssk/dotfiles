1a3730e3181d4f37752f1b72be3f329962a56783
mstssk <mstssk+github@gmail.com>
2018-11-07 16:17:54 +0900

    :+1: コミットテンプレートを追加

--- setup-ln.sh
+++ setup-ln.sh
@@ -6,3 +6,4 @@ ln -s $HOME/dotfiles/.zsh $HOME/
 ln -s $HOME/dotfiles/.zshrc $HOME/
 ln -s $HOME/dotfiles/.gitconfig $HOME/
 ln -s $HOME/dotfiles/.global_ignore $HOME/
+ln -s $HOME/dotfiles/.git-commit.template $HOME/
