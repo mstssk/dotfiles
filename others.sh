7356ca6a4935f3a8da60d1f0550b9b60fc9fbac7
mstssk <mstssk+github@gmail.com>
2019-01-09 00:42:02 +0900

    goappにパーミッション付ける

--- others.sh
+++ others.sh
@@ -10,6 +10,7 @@ mkdir -p $DEV_DIR
 wget -O $DEV_DIR/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-196.0.0-darwin-x86_64.tar.gz
 tar xvzf $DEV_DIR/google-cloud-sdk.tar.gz -C $DEV_DIR
 $DEV_DIR/google-cloud-sdk/install.sh --quiet
-gcloud components update
+gcloud components update --quiet
 gcloud components install app-engine-go --quiet
+chmod +x $DEV_DIR/google-cloud-sdk/platform/google_appengine/goapp
 # gcloud components install beta -q
