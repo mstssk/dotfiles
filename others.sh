40a3b0d1f3f7dbfc306b47d1d83256e99862ef90
mstssk <mstssk@users.noreply.github.com>
2019-06-03 11:38:44 +0900

    Update gcloud sdk version

--- others.sh
+++ others.sh
@@ -7,7 +7,7 @@ DEV_DIR=~/Development
 mkdir -p $DEV_DIR
 
 # Google Cloud SDK
-wget -O $DEV_DIR/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-196.0.0-darwin-x86_64.tar.gz
+wget -O $DEV_DIR/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-248.0.0-darwin-x86_64.tar.gz
 tar xvzf $DEV_DIR/google-cloud-sdk.tar.gz -C $DEV_DIR
 $DEV_DIR/google-cloud-sdk/install.sh --quiet
 gcloud components update --quiet
