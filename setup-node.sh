7d731b5fc618da627812adc1cc92fd5411c5b10d
mstssk <mstssk@users.noreply.github.com>
2020-03-03 11:37:19 +0900

    最初に入れるNode.jsをv12 LTSに更新

--- setup-node.sh
+++ setup-node.sh
@@ -3,8 +3,8 @@
 # https://github.com/hokaccha/nodebrew
 curl -L git.io/nodebrew | perl - setup
 nodebrew -v
-nodebrew install-binary v10
-nodebrew use v10
+nodebrew install-binary v12
+nodebrew use v12
 
 npm config set init.license MIT
 
