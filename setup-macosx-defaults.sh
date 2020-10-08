892da5f15d63363d07cf8a378b3905c9b9532610
mstssk <mstssk@users.noreply.github.com>
2020-10-08 17:20:44 +0900

    スクリーンショットでウィンドウに影を付けない

--- setup-macosx-defaults.sh
+++ setup-macosx-defaults.sh
@@ -4,3 +4,6 @@ cd `dirname $0`
 set -x
 
 defaults write com.google.Chrome NSRequiresAquaSystemAppearance -bool Yes
+defaults write com.apple.screencapture disable-shadow -boolean true
+
+killall SystemUIServer
