504c071871929db1844f30a78f63f3454e559f5b
mstssk <mstssk+github@gmail.com>
2020-12-25 22:01:56 +0900

    Use DarkMode force for ComicViewer

--- setup-macosx-defaults.sh
+++ setup-macosx-defaults.sh
@@ -4,6 +4,7 @@ cd `dirname $0`
 set -x
 
 defaults write com.google.Chrome NSRequiresAquaSystemAppearance -bool Yes
+defaults write org.cathand.comicviewer NSRequiresAquaSystemAppearance -bool No
 defaults write com.apple.screencapture disable-shadow -boolean true
 
 killall SystemUIServer
