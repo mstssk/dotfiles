99688d2b7c7755d6d804c07048e69555c1cf9282
mstssk <mstssk+github@gmail.com>
2021-07-02 15:36:50 +0900

    karabinerを使わなくなった

--- setup-ln.sh
+++ setup-ln.sh
@@ -15,5 +15,5 @@ ln -s $HOME/dotfiles/.zshrc $HOME/
 ln -s $HOME/dotfiles/.gitconfig $HOME/
 ln -s $HOME/dotfiles/.global_ignore $HOME/
 ln -s $HOME/dotfiles/.git-commit.template $HOME/
-_lndir $HOME/dotfiles/.config/karabiner/ $HOME/.config/karabiner
+# _lndir $HOME/dotfiles/.config/karabiner/ $HOME/.config/karabiner
 
