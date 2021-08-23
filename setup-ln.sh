c59d58c5a060e95fd804a27dcdbc1de98fe631d3
mstssk <mstssk+github@gmail.com>
2021-08-23 22:31:55 +0900

    ちゃんと多重リンク回避を使う

--- setup-ln.sh
+++ setup-ln.sh
@@ -2,6 +2,11 @@
 
 cd `dirname $0`
 
+# ディレクトリ用のシンボリックリンク作成関数
+#
+# ディレクトリのシンボリックリンクにもう一度 ln -s すると、
+# エラーや上書きにはならずリンク先ディレクトリに再起的にリンクを作成されてしまう。
+# ディレクトリの存在チェックを行うことで多重処理を防ぐ。
 function _lndir() {
   if [ -e "$2" ]; then
     echo "$2: Dir or SymLink already exists"
@@ -12,5 +17,5 @@ function _lndir() {
 
 ln -s $HOME/dotfiles/.zsh $HOME/
 ln -s $HOME/dotfiles/.zshrc $HOME/
-ln -s $HOME/dotfiles/.config/git $HOME/.config/git
+_lndir $HOME/dotfiles/.config/git/ $HOME/.config/git
 # _lndir $HOME/dotfiles/.config/karabiner/ $HOME/.config/karabiner
