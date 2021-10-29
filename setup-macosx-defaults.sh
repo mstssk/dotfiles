81209ff577e7c9c725f3b8ab777c46b9c6f6c406
mstssk <mstssk+github@gmail.com>
2021-10-29 16:06:50 +0900

    コミックビューアーもう使っていない

--- setup-macosx-defaults.sh
+++ setup-macosx-defaults.sh
@@ -4,7 +4,6 @@ cd `dirname $0`
 set -x
 
 defaults write com.google.Chrome NSRequiresAquaSystemAppearance -bool Yes
-defaults write org.cathand.comicviewer NSRequiresAquaSystemAppearance -bool No
 defaults write com.apple.screencapture disable-shadow -boolean true
 
 killall SystemUIServer
